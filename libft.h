121c18d1c5859511f748220fd324a2723a4eb947
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2024-09-11 20:27:31 +0200

    Cabeceras

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 19:46:37 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/09/11 20:25:12 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+
+/* Libft basic functions*/
+
+int	ft_isalpha(int c);
+
+#endif
