3fd44e4a590a52e2d23aaa0a00d0a5dad5b287f7
Juan Carlos Alcausa Luque <jalcausa@c2r3s5.42malaga.com>
2024-09-12 19:06:13 +0200

    ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 18:44:47 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/09/12 19:05:53 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Return the length of a given string
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	while (str[i])
+		++i;
+	return (i);
+}
+/*
+#include <stdio.h>
+int	main (void)
+{
+	printf("%zu\n", ft_strlen("Hola"));
+	return (0);
+}
+*/
