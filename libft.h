c953c9093a8ba72463e89affcaecb42b4906a14d
Juan Carlos Alcausa Luque <jalcausa@c2r3s5.42malaga.com>
2024-09-12 19:16:56 +0200

    Correccion en el .h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:46:37 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/12 19:13:54 by jalcausa         ###   ########.fr       */
+/*   Updated: 2024/09/12 19:15:30 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *str);
 
 #endif
