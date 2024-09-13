9cc26ff9f3969bd804f06c93639e28da23369e62
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2024-09-13 14:53:54 +0200

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/13 13:06:16 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/13 14:49:07 by jalcausa         ###   ########.fr       */
+/*   Created: 2024/09/13 14:47:06 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/09/13 14:51:32 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*The memset() function writes len bytes of value c 
+/*The bzero() function writes len bytes of value zero 
 (converted to an unsigned char) to the string s. */
-void	*ft_memset(void *s, int c, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
-	while (i < len)
+	while (i < n)
 	{
-		((char *)s)[i] = (unsigned char)c;
+		((char *)s)[i] = (unsigned char)0;
 		++i;
 	}
-	return (s);
 }
