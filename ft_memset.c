d5e2d5b4d449db689cec9932eda396675375672b
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2024-09-13 13:48:49 +0200

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/12 18:44:47 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/13 13:44:38 by jalcausa         ###   ########.fr       */
+/*   Created: 2024/09/13 13:06:16 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/09/13 13:48:23 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Return the length of a given string
-size_t	ft_strlen(const char *str)
+/*The memset() function writes len bytes of value c 
+(converted to an unsigned char) to the string b. */
+void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	while (str[i])
+	while (i < len)
+	{
+		((char *)b)[i] = (unsigned char)c;
 		++i;
-	return (i);
+	}
+	return (b);
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%zu\n", ft_strlen("Hola"));
-	return (0);
-}
-*/
