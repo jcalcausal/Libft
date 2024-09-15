4134ed170dd688cf334f1b19d41c517986e77ce7
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2024-09-15 13:11:43 +0200

    ft_memmove.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:46:37 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/13 18:34:51 by jalcausa         ###   ########.fr       */
+/*   Updated: 2024/09/15 13:11:19 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
 
 #endif
