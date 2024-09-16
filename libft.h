8db543467632c863ae9c92359b67035ab121e447
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2024-09-16 12:46:22 +0200

    ft_strchr.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:46:37 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/15 14:08:31 by jalcausa         ###   ########.fr       */
+/*   Updated: 2024/09/16 12:46:08 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int l);
 int		ft_tolower(int l);
+char	*ft_strchr(const char *s, int c);
 
 #endif
