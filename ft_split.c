9ed06e95972626362cf334ce8551419d71f6b3d6
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2024-09-18 09:21:54 +0200

    Inicio ft_split.c

---
+++ ft_split.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 09:17:25 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/09/18 09:18:55 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+Divides the given string s in words using as a delimiter the char c and
+returning an array of strings (words) using malloc
+*/
+char **ft_split(char const *s, char c)
+{
+	
+}
