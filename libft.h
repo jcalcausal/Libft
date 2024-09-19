cb3b9ff78103a256e795c2f926cc43eb6785efa3
Juan Carlos Alcausa Luque <jcalcausal@IMac-jcalcausa-2.local>
2024-09-19 11:13:43 +0200

    Añadir ft_isprint al archivo de cabeceras

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:46:37 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/19 11:04:36 by jalcausa         ###   ########.fr       */
+/*   Updated: 2024/09/19 11:11:01 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
