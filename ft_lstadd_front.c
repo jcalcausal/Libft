f40a7b1a1f0ef9bfa9384c41bb2966ccda9d5bda
Juan Carlos Alcausa Luque <jalcausa@c3r5s4.42malaga.com>
2024-09-21 17:58:15 +0200

    Error tipográfico

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,14 +6,14 @@
 /*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 09:35:05 by jalcausa          #+#    #+#             */
-/*   Updated: 2024/09/20 09:43:49 by jalcausa         ###   ########.fr       */
+/*   Updated: 2024/09/21 17:57:10 by jalcausa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-Adds node new as the fisr node of a list. First it changes the next element of
+Adds node new as the first node of a list. First it changes the next element of
 new to the previous first element of the list and then changes the pointer lst
 (that determines the beginning of the list) to new, the new first element
 */
