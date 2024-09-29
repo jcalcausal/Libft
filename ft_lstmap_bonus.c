5adcee06194f41b2608f56f7636250dae6e5cce7
Juan Carlos Alcausa Luque <160739943+jcalcausal@users.noreply.github.com>
2024-09-29 18:51:34 +0200

    Corección en ft_lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -20,15 +20,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
+			if (new_content)
+				del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
